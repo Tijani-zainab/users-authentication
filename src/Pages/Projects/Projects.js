f13fa8b07f0cf3cb25dfac893135d12304493dc6
Timothy-Achonu <achonutimothy@gmail.com>
2022-11-23 16:37:11 +0100

    added an extra folder and changed css files to module.css files

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,9 +1,10 @@
-import './Projects.css';
+import styles from './Projects.module.css';
 
 const Projects = () => {
     return (
         <div className="Projects">
             <h1>Projects</h1>
+        <p className={styles.projectStyle}>Here are a few projects we have worked on both individually and collaboratively</p>
         </div>
     );
 }
