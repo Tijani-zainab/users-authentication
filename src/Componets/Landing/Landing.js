5378e3ec52e8ccf862aa457ffb727bec33f910fa
Zainab Tijani <jani.tijj@gmail.com>
2022-11-23 15:27:52 +0100

    add file templates to the file structure

---
+++ src/Componets/Landing/Landing.js
@@ -0,0 +1,12 @@
+import './Landing.css';
+
+const Landing = () => {
+
+    return (
+        <div className="Landing">
+            <h1>Landing</h1>
+        </div>
+    );
+}
+
+export default Landing;
---
+++ src/Componets/Routing/Routing.js
@@ -0,0 +1,11 @@
+import './Routing.css';
+
+const Routing = () => {
+    return (
+        <div className="Routing">
+            <h1>Routing</h1>
+        </div>
+    );
+}
+
+export default Routing;
